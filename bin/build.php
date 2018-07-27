d4522c40dc0b0e1ca918d5939372a020751570db
ivan.atanasov <ivan.atanasov@CPX-JYRS3GF39CP.dir.svc.accenture.com>
2018-07-27 15:23:12 +0300

    Fix build script

--- bin/build.php
+++ bin/build.php
@@ -6,6 +6,6 @@ $mkpharBin	= 'php -dphar.readonly=0 /usr/local/bin/mkphar';
 
 posix_getuid() === 0 || die( "Run it as root user.\n");
 
-exec( sprintf( "%s %s %s.phar main.php 2>&1 &", $mkpharBin, __DIR__, $target ) );
+exec( sprintf( "%s %s %s.phar main.php 2>&1 &", $mkpharBin, __DIR__/src, $target ) );
 rename( $target.'.phar', $target );
 chmod( $target, 0777 );
